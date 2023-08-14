d03ca50a865756519fecbf8cff282945eabb7ab2
Joe Marini <cjmarini68@gmail.com>
2023-08-14 21:36:41 +0000

    reset start state

--- Start/Ch2/RequiredProps/Program.cs
+++ Start/Ch2/RequiredProps/Program.cs
@@ -7,7 +7,7 @@ Employee emp2 = new Employee() {FirstName="Jane", LastName="Deaux", ID=2, Depart
 Employee emp3 = new Employee() {FirstName="Jim", LastName="Dough", ID=3, Department="Marketing"};
 
 // TODO: Construct an object using the constructor
-Employee emp4 = new Employee("Joe", "Doh", 4, "Marketing");
+
 
 Console.WriteLine(emp1);
 Console.WriteLine(emp2);
