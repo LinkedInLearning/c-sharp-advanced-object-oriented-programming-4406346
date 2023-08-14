ea40eafc0b7de627865c233038480027f671c552
Joe Marini <cjmarini68@gmail.com>
2023-08-14 23:29:50 +0000

    challenge start state

--- Start/Ch3/Challenge/Program.cs
+++ Start/Ch3/Challenge/Program.cs
@@ -1,18 +1,18 @@
 ﻿// Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Challenge: create a class hierarchy to represent employees
 
-// Test code for the programming challenge
-var Emp1 = new HourlyEmployee() {FullName="John Doe", Department="Sales", ID=1, PayRate=35.0m};
-var Emp2 = new SalariedEmployee() {FullName="Jane Deaux", Department="Marketing", ID=2, Salary=75000.0m};
+var Emp1 = new HourlyEmployee() {FullName="John Doe", Department="Sales", PayRate=35.0m};
+var Emp2 = new SalariedEmployee() {FullName="Jane Deaux", Department="Marketing", Salary=75000.0m};
 
-Console.WriteLine($"{Emp1} -- {Emp1.PayRate:C2}");
-Emp1.Department = "Legal";
-Emp1.AdjustPay(0.10m);
-Console.WriteLine($"{Emp1} -- {Emp1.PayRate:C2}");
+Console.WriteLine($"Count: {Employee.EmployeeCount}");
 
-Console.WriteLine("-------------------");
+var Emp3 = new SalariedEmployee() {FullName="Jenny Doh", Department="Engineering", Salary=95000.0m};
+Console.WriteLine($"Count: {Employee.EmployeeCount}");
 
-Console.WriteLine($"{Emp2} -- {Emp2.Salary:C2}");
-Emp2.Department = "Solution Architecture";
-Emp2.AdjustPay(0.07m);
-Console.WriteLine($"{Emp2} -- {Emp2.Salary:C2}");
+var Emp4 = new HourlyEmployee() {FullName="Jim Dough", Department="Tech Support", PayRate=45.0m};
+Console.WriteLine($"Count: {Employee.EmployeeCount}");
+
+Console.WriteLine($"{Emp1}");
+Console.WriteLine($"{Emp2}");
+Console.WriteLine($"{Emp3}");
+Console.WriteLine($"{Emp4}");
