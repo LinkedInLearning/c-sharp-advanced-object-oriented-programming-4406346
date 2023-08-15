cbe6c0f2046462ae017382bd6684e810e040a5b5
Joe Marini <cjmarini68@gmail.com>
2023-08-15 20:27:05 +0000

    update start state

--- Start/Ch4/SealedClasses/Program.cs
+++ Start/Ch4/SealedClasses/Program.cs
@@ -1,9 +1,8 @@
 ﻿// Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Sealed classes and members
 
-// Create a class derived from A
+// TODO: Seal a class
 B example1 = new B();
 example1.Func1();
 
-E example2 = new E();
-example2.Func2();
+// TODO: Seal a method
