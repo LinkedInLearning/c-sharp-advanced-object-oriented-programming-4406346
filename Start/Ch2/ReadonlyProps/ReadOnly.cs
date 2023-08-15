02f4154924097e4b466caa3e39787b59cbb63824
Joe Marini <cjmarini68@gmail.com>
2023-08-15 20:56:51 +0000

    start state

--- Start/Ch2/ReadonlyProps/ReadOnly.cs
+++ Start/Ch2/ReadonlyProps/ReadOnly.cs
@@ -3,7 +3,7 @@
 
 public class Book {
     // Declare some private fields
-    private readonly string _ISBN = "";
+    private string _ISBN = "";
     private string _title = "";
     private string _author = "";
 
@@ -14,7 +14,7 @@ public class Book {
     }
 
     public void Update(string ISBN, string Title, string Author) {
-        // _ISBN = ISBN; // causes a compile error
+        _ISBN = ISBN;
         _title = Title;
         _author = Author;
     }
