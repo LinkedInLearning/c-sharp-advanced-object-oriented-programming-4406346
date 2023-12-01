ce18f6b7f94a04ec0ddc9f9fcfda14d0fd9bdd36
rahulbcsit <rahulbcsit@users.noreply.github.com>
2023-12-01 04:45:54 +0000

    commit for the inheritance

--- Start/Ch1/Inheritance/Program.cs
+++ Start/Ch1/Inheritance/Program.cs
@@ -5,6 +5,12 @@
 Circle c = new Circle(10);
 Rectangle r = new Rectangle(10,20);
 
+// Console.WriteLine($"{c}");
+// System.Console.WriteLine($"{r}");
+
+
+// Console.WriteLine($"{c is Shape2D}");
+// System.Console.WriteLine($"{c is Rectangle}");
 // TODO: Exercise the ToString() method
 
 
@@ -13,9 +19,11 @@ Rectangle r = new Rectangle(10,20);
 
 // TODO: Call the GetArea() function on each one
 
-
+Square s = new Square(10);
+Console.WriteLine(s.GetArea());
 // TODO: Print the area of each shape
 
 
 // TODO: All of the classes derive from Shape2D, so we can treat each one
 // as an instance of the base class. 
+
