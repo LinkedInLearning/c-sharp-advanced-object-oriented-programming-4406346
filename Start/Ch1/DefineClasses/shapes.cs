fa5fecd4efcd708284e6ae1b4d28845375b7f0bf
rahulbcsit <rahulbcsit@users.noreply.github.com>
2023-12-01 04:10:36 +0000

    initial commit

--- Start/Ch1/DefineClasses/shapes.cs
+++ Start/Ch1/DefineClasses/shapes.cs
@@ -2,15 +2,33 @@
 // Defining and instantiating classes
 
 // TODO: Define a class using the "class" keyword
+public class Rectangle
+{
 
+    public Rectangle(int w, int h)
+    {
+        width = w;
+        height = h;
+    }
     // TODO: The constructor accepts parameters used to create the object
 
+
+    public Rectangle(int side)
+    {
+        width = height = side;
+    }
 
     // TODO: For convenience, we can have a constructor that takes one value
     // for squares that have the same side size
 
 
     // TODO: Classes can define methods that return values
-
+    public int GetArea()
+    {
+        return width * height;
+    }
 
     // TODO: member variables hold data
+    public int width;
+    public int height;
+}
